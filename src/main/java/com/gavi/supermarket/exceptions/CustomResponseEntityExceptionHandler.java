616e6857cffc7fd16a7bccf2f88ab7abcf5a0856
Gavishiddappa <gmgmadagi@gmail.com>
2018-01-24 00:59:23 +0530

    Added Exception Handler for all exceptions

--- src/main/java/com/gavi/supermarket/exceptions/CustomResponseEntityExceptionHandler.java
+++ src/main/java/com/gavi/supermarket/exceptions/CustomResponseEntityExceptionHandler.java
@@ -1,7 +1,27 @@
 package com.gavi.supermarket.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
 /**
  * Created by Hi on 24-01-2018.
  */
-public class CustomResponseEntityExceptionHandler {
+
+@ControllerAdvice
+@RestController
+public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) {
+        ExceptionResponse exceptionResponse =  new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<Object>(exceptionResponse, HttpStatus.NOT_FOUND);
+    };
 }
--- src/main/java/com/gavi/supermarket/exceptions/ExceptionResponse.java
+++ src/main/java/com/gavi/supermarket/exceptions/ExceptionResponse.java
@@ -1,7 +1,31 @@
 package com.gavi.supermarket.exceptions;
 
+import java.util.Date;
+
 /**
  * Created by Hi on 24-01-2018.
  */
 public class ExceptionResponse {
+
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    public ExceptionResponse(Date timestamp, String message, String details) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
 }
